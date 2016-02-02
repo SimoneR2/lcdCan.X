d9c2591181857fceae4c9a9886d36ef15a173c60
Simone <simone.righetti97@gmail.com>
2016-02-02 17:21:51 +0100

    First edition

---
+++ build/default/production/lcdCan.p1.d
@@ -0,0 +1,10 @@
+ build/default/production/lcdCan.d  \
+ build/default/production/lcdCan.p1:  \
+ lcdCan.c  \
+idCan.h  \
+I:/Tesina/LaurTec_PIC_libraries_v_3.3.0/inc/delay.h  \
+pic18f4480_config.h  \
+I:/Tesina/LaurTec_PIC_libraries_v_3.3.0/inc/CANlib.h  \
+I:/Tesina/LaurTec_PIC_libraries_v_3.3.0/inc/LCD_44780.h  \
+I:/Tesina/LaurTec_PIC_libraries_v_3.3.0/src/delay.c  \
+I:/Tesina/LaurTec_PIC_libraries_v_3.3.0/src/LCD_44780.c 
---
+++ lcdCan.c
@@ -0,0 +1,47 @@
+#include <xc.h>
+#define LCD_DEFAULT
+#include "CANlib.h"
+#include "delay.h"
+#include "delay.c"
+#include "LCD_44780.h" 
+#include "LCD_44780.c"
+#include "idCan.h"
+#include "pic18f4480_config.h"
+CANmessage msg;
+
+__interrupt(high_priority) void ISR_Alta(void) {
+    if (CANisRxReady()) { //Se il messaggio è arrivato
+        CANreceiveMessage(&msg); //leggilo e salvalo
+        PIR3bits.RXB0IF = 0;
+        PIR3bits.RXB1IF = 0;
+    }
+}
+void configurazione(void);
+
+void main(void) {
+    configurazione();
+    LCD_initialize(16); 
+    while (1){
+//        LCD_write_string(msg.identifier);
+    }
+}
+
+void configurazione(void) {
+    CANInitialize(4, 6, 5, 1, 3, CAN_CONFIG_LINE_FILTER_OFF & CAN_CONFIG_SAMPLE_ONCE & CAN_CONFIG_ALL_VALID_MSG & CAN_CONFIG_DBL_BUFFER_ON);
+    LATA = 0x00;
+    TRISA = 0xff; //ALL IN
+    LATB = 0x00;
+    TRISB = 0xff; //RB1 e RB2 INPUT
+    LATC = 0x00;
+    TRISC = 0xff;
+    LATD = 0x00;
+    TRISD = 0x00; //OUTPUT LCD
+    LATE = 0x00;
+    TRISE = 0xFF;
+    PIR3bits.RXB1IF = 0; //azzera flag interrupt can bus buffer1
+    PIR3bits.RXB0IF = 0; //azzera flag interrupt can bus buffer0
+    IPR3bits.RXB1IP = 1; //interrupt alta priorità per can
+    IPR3bits.RXB0IP = 1; //interrupt alta priorità per can
+    PIE3bits.RXB1IE = 1; //abilita interrupt ricezione can bus buffer1
+    PIE3bits.RXB0IE = 1; //abilita interrupt ricezione can bus buffer0
+}
